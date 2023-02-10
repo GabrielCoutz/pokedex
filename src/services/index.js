31986e854a8e3eb75a73467f55007dc08d02b8fd
Gabriel Main <gabriel.coutinho.cassiano@gmail.com>
2023-02-09 21:19:13 -0300

    Todas evoluções do pokemon armazenada

--- src/services/index.js
+++ src/services/index.js
@@ -21,8 +21,43 @@ export async function getPokemonEvolution(pokemon) {
   );
   const { url } = pokemonData.evolution_chain;
   const { data } = await axios.get(url);
+
+  if (hasEvolve(data.chain.evolves_to)) {
+    const a = getEvolutions(data.chain.evolves_to);
+    console.log(a);
+  }
+
   const [{ evolves_to }] = data.chain.evolves_to;
   const evolutionName = evolves_to[0].species.name;
   const { data: evolutionImg } = await axiosInstance.get(evolutionName);
   return [{ name: evolutionName, img: evolutionImg.sprites.front_default }];
+}
+
+function hasEvolve(item) {
+  return item.length;
+}
+
+function getEvolutions([pokemon]) {
+  const evoChain = [];
+  let evoData = pokemon;
+
+  do {
+    evoChain.push(evoData.species);
+
+    evoData = evoData["evolves_to"][0];
+  } while (!!evoData && evoData.hasOwnProperty("evolves_to"));
+
+  // if (!hasEvolve(pokemon[0].evolves_to))
+  //   return evolutions.push(pokemon.species);
+
+  // const teste = pokemon.map((item) => {
+  //   if (hasEvolve(item.evolves_to)) return getEvolutions(item.evolves_to);
+  // });
+
+  // console.log(teste);
+
+  // evolutions.push(pokemon.evolves_to[0].species);
+  // getEvolutions(pokemon.evolves_to);
+
+  return evoChain;
 }
